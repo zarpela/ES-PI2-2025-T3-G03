2d8c637fa170bc7cf0196332b7b9e0ecc6358345
MurilloCaravita <murillo.caravita@gmail.com>
2025-09-14 18:13:20 -0300

    tela cadastrar usuario

--- src/notadez.ts
+++ src/notadez.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import path from 'path';
 
-import indexRoutes from "./index/controller.ts";
+import indexRoutes from "./index/controller.ts";     // Importa rota index
+import usuarioRoutes from "./usuario/controller.ts"; // Importa rota usuário
 
 const app = express();
 const port = 3000;
 
-app.use(indexRoutes);
+app.use(indexRoutes);   // Usa rota index
+app.use(usuarioRoutes); // Usa rota usuário
 
 app.use('/public', express.static(path.resolve('public')));
 
