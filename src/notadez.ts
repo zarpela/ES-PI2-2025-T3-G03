2890fbaf2ca1da4776553deb7b8564e597ddf3ca
MurilloCaravita <murillo.caravita@gmail.com>
2025-09-14 17:46:19 -0300

    Tela de Login e .ejs

--- src/notadez.ts
+++ src/notadez.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import path from 'path';
+
+import indexRoutes from "./index/controller.ts";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Olá, NotaDez!");
-});
+app.use(indexRoutes);
+
+app.use('/public', express.static(path.resolve('public')));
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
