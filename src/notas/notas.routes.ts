dc2932ca448f730b678e223ec0840de553f5389e
zarpela <marcelinhozarpelon@gmail.com>
2025-11-16 15:03:12 -0300

    arruma importacao csv
    troca o endpoint de importacao para
    POST '/turma/:turmaId/importar-alunos-csv',

--- src/notas/notas.routes.ts
+++ src/notas/notas.routes.ts
@@ -1,20 +1,17 @@
 //Desenvolvido por Marcelo
 import { Router } from 'express';
 import multer from 'multer';
-import { importarCsvController, exportarCsvController } from './notas.controller.ts'; // Ou onde seu controller estiver
+import { importarAlunosCsvController, exportarCsvController } from './notas.controller.ts';
 
 const notaRoutes = Router();
 
-// Configura o Multer para usar a memória
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Define a rota de importação
-// 'file' é o nome do campo que o front-end enviará
 notaRoutes.post(
-  '/importar-csv',       
+  '/turma/:turmaId/importar-alunos-csv',       
   upload.single('file'), 
-  importarCsvController  
+  importarAlunosCsvController  
 );
 
 notaRoutes.get(
